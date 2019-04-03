0f75711b8761c07a72cea204fcf1202fd54f0637
fauzanradifansk <48641808+fauzanradifansk@users.noreply.github.com>
2019-04-03 15:40:23 +0700

    Update Dashboard

--- controllers/Dasboard.php
+++ controllers/Dasboard.php
@@ -5,4 +5,26 @@ class Dasboard extends CI_Controller {
         {
                 $this->load->view('dasboard_user');
         }
+        public function peminjaman_barang_user(){
+                $this->load->view('peminjaman-barang_user');
+        }
+        public function peminjaman_data_user(){
+                $this->load->view('peminjaman-data_user');
+        }
+        public function detail_peminjaman_user(){
+                $this->load->view('detail-peminjaman_user');
+        }
+        public function detail_peminjaman2_user(){
+                $this->load->view('detail-peminjaman2_user');
+        }
+        public function pemberitahuan_user(){
+                $this->load->view('pemberitahuan_user');
+        }
+        function logout(){
+		$this->session->sess_destroy();
+		redirect(base_url());
+        }
+        public function detail_barang_user(){
+                $this->load->view('detail-barang_user');
+        }
 }
